257640dfc61623ebc1e4c4511533351596511d44
Patrick Goebel <patrick@pirobot.org>
2016-09-23 05:10:17 -0700

    First commit

---
+++ jr_speech/nodes/say_phrases.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+
+"""
+    say_phrases.py - Version 1.1 2013-12-20
+    
+    Use the sound_play client play text-to-speech or pre-recorded wav files.
+    
+    Created for the Pi Robot Project: http://www.pirobot.org
+    Copyright (c) 2016 Patrick Goebel.  All rights reserved.
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.5
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details at:
+    
+    http://www.gnu.org/licenses/gpl.htmlPoint
+"""
+
+import rospy
+from jr_msgs.srv import SayPhrase
+from sound_play.libsoundplay import SoundClient
+import sys
+from yaml import load
+
+class SayPhrases:
+    def __init__(self, script_path):
+        rospy.init_node('say_phrases')
+
+        rospy.on_shutdown(self.cleanup)
+        
+        # Get the path of the configuration file
+        config_file = rospy.get_param('~config_file', None)
+        
+        # Use text-to-speech or play wave files?
+        self.use_tts = rospy.get_param("~use_tts", True)
+        
+        # Play random phrases or listen on a service for a signal?
+        self.random_phrases = rospy.get_param("~random_phrases", True)
+        
+        # Set the default TTS voice to use
+        self.voice = rospy.get_param("~voice", "voice_don_diphone")
+        
+        # Set the wave file path if used
+        self.wavepath = rospy.get_param("~wavepath", script_path + "/../wave_files")
+        
+        self.phrases = rospy.get_param("~phrases", None)
+        
+        # Load the configuration parameters
+        with open(config_file, 'r') as config:
+            self.phrases = load(config)
+        
+        rospy.loginfo(self.phrases)
+        
+        # Create the sound client object
+        self.soundhandle = SoundClient()
+        
+        # Wait a moment to let the client connect to the sound_play server
+        rospy.sleep(1)
+        
+        # Make sure any lingering sound_play processes are stopped.
+        self.soundhandle.stopAll()
+        
+        # Announce that we are ready for input
+        self.soundhandle.playWave(self.wavepath + "/R2D2a.wav")
+        #rospy.sleep(1)
+        #self.soundhandle.say("Ready", self.voice)
+        
+        if self.random_phrases:  
+            rospy.loginfo("Saying random phrases...")
+        else:
+            # Create a service to accept location requests
+            rospy.Service('~say_phrase', SayPhrase, self.SayPhraseHandler)
+            
+            rospy.loginfo("Waiting for signal to say something...")
+        
+    def SayPhraseHandler(self, req):
+        id = req.data
+        
+        rospy.loginfo(id)
+            
+#         if id > len(self.phrases) - 1:
+#             return
+        
+        rospy.loginfo(self.phrases)
+        
+        [phrase] = [phrase['phrase'] for phrase in self.phrases if phrase['id'] == id]
+        
+        rospy.loginfo(phrase)
+        
+        self.soundhandle.say(str(phrase), self.voice)
+
+    def cleanup(self):
+        self.soundhandle.stopAll()
+        rospy.loginfo("Shutting down talkback node...")
+
+if __name__=="__main__":
+    try:
+        SayPhrases(sys.path[0])
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        rospy.loginfo("Say phrases node terminated.")
---
+++ jr_utils/src/jr_utils/utilities.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+""" utilities.py - Version 1.0 2016-09-11
+
+    Utilities for the JackRabbot project
+
+    Created for the Pi Robot Project: http://www.pirobot.org
+    Copyright (c) 2016 Patrick Goebel.  All rights reserved.
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.5
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details at:
+    
+    http://www.gnu.org/licenses/gpl.html
+      
+"""
+
+import rospy
+from visualization_msgs.msg import Marker, MarkerArray
+from copy import deepcopy
+
+def create_location_markers(marker_scale=0.2, frame_id='map', locations=None, show_labels=True):
+    # Define a marker publisher.
+    location_marker_pub = rospy.Publisher('location_markers', MarkerArray, queue_size=5)
+    
+    # Initialize the marker array
+    location_markers = MarkerArray()
+    
+    # Create a marker for each location
+    for location in locations:
+        location_marker = Marker()
+        
+        # Fill in the common properties
+        marker_lifetime = 0   # 0 is forever
+        marker_ns = 'locations'
+        marker_color = {'r': 1.0, 'g': 1.0, 'b': 0.0, 'a': 1.0}
+    
+        location_marker.ns = marker_ns
+        location_marker.header.frame_id = frame_id
+        location_marker.header.stamp = rospy.Time.now()
+        location_marker.type = Marker.CUBE
+        location_marker.action = Marker.ADD
+        location_marker.lifetime = rospy.Duration(marker_lifetime)
+        location_marker.scale.x = marker_scale
+        location_marker.scale.y = marker_scale
+        location_marker.scale.z = 0.02
+        location_marker.color.r = marker_color['r']
+        location_marker.color.g = marker_color['g']
+        location_marker.color.b = marker_color['b']
+        location_marker.color.a = marker_color['a']
+        
+        location_marker.id = location['id']
+        location_marker.pose = location['pose'].pose
+        location_marker.text = location['name']
+        
+        location_markers.markers.append(location_marker)
+        
+        if show_labels:
+            label_marker = Marker()
+            label_marker = deepcopy(location_marker)
+            label_marker.id = 100 + int(location_marker.id)
+            label_marker.type = Marker.TEXT_VIEW_FACING
+            label_marker.scale.z = marker_scale
+            label_marker.pose.position.x = location_marker.pose.position.x + 0.5
+        
+            location_markers.markers.append(label_marker)
+                
+    return location_marker_pub, location_markers
+
+    
